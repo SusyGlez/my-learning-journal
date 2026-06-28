adc51c8fbccc32eaa8c225fb2a8e900da8ebc4db
SusyGlez <susanaglezb@gmail.com>
2026-06-28 17:47:22 +0200

    created hero-post.html file and added content in it.

--- posts.js
+++ posts.js
@@ -6,4 +6,44 @@ export const postsArray = [
     post: "I'm excited to start a new learning journey as a Scrimba Bootcamp student! After several months of learning in the Frontend Developer Career Path.",
     id: 0,
   },
+
+  {
+    image: `images/blog-image-04.png`,
+    date: "JULY 23, 2022",
+    title: "Blog two",
+    post: "I'm excited to start a new learning journey as a Scrimba Bootcamp student! After several months of learning in the Frontend Developer Career Path.",
+    id: 1,
+  },
+
+  {
+    image: `images/blog-image-05.png`,
+    date: "JULY 23, 2022",
+    title: "Blog three",
+    post: "I'm excited to start a new learning journey as a Scrimba Bootcamp student! After several months of learning in the Frontend Developer Career Path.",
+    id: 2,
+  },
+
+  {
+    image: `images/blog-image-03.png`,
+    date: "JULY 23, 2022",
+    title: "Blog four",
+    post: "I'm excited to start a new learning journey as a Scrimba Bootcamp student! After several months of learning in the Frontend Developer Career Path.",
+    id: 3,
+  },
+
+  {
+    image: `images/blog-image-01.png`,
+    date: "JULY 23, 2022",
+    title: "Blog five",
+    post: "I'm excited to start a new learning journey as a Scrimba Bootcamp student! After several months of learning in the Frontend Developer Career Path.",
+    id: 4,
+  },
+
+  {
+    image: `images/blog-image-02.png`,
+    date: "JULY 23, 2022",
+    title: "Blog six",
+    post: "I'm excited to start a new learning journey as a Scrimba Bootcamp student! After several months of learning in the Frontend Developer Career Path.",
+    id: 5,
+  },
 ];
