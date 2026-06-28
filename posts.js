74644d0810268ddd9feef176331e700a042a4e89
SusyGlez <susanaglezb@gmail.com>
2026-06-28 17:01:31 +0200

    created posts.js file and added first content in it. Addded images

---
+++ posts.js
@@ -0,0 +1,9 @@
+export const postsArray = [
+  {
+    image: `images/blog-image-06.png`,
+    date: "JULY 23, 2022",
+    title: "Blog one",
+    post: "I'm excited to start a new learning journey as a Scrimba Bootcamp student! After several months of learning in the Frontend Developer Career Path.",
+    id: 0,
+  },
+];
