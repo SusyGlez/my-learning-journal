9a4812be8aad33286d956b07386136b5d07a1c81
SusyGlez <susanaglezb@gmail.com>
2026-07-01 16:32:18 +0200

    added css to hero post page

--- index.js
+++ index.js
@@ -2,21 +2,29 @@ import { postsArray } from "./posts.js";
 
 const viewMoreBtn = document.getElementById("nav-link-viewmore");
 
-viewMoreBtn.addEventListener("click", function () {});
+let showAll = false;
 
 function getPostsListHtml() {
   let postsListHtml = ``;
 
-  postsArray.forEach(function (post) {
+  const postToShow = showAll ? postsArray : postsArray.slice(0, 3);
+
+  postToShow.forEach(function (post) {
     postsListHtml += `<div class="post-container">
-            <img class="post-img" src="${post.image}"/>
-            <p class="post-date">${post.date}</p>
-            <h8 class="post-title">${post.title}</h8>
-            <p class="post-content">${post.content}</p>
+        <img class="post-img" src="${post.image}"/>
+        <p class="post-date">${post.date}</p>
+        <h8 class="post-title">${post.title}</h8>
+        <p class="post-content">${post.content}</p>
         </div>`;
   });
   return postsListHtml;
 }
+
+viewMoreBtn.addEventListener("click", function () {
+  showAll = true;
+  renderPostsList();
+  viewMoreBtn.style.display = "none";
+});
 
 function renderPostsList() {
   document.getElementById("other-content").innerHTML = getPostsListHtml();
